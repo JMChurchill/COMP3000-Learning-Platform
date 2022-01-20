736c490b4e8186619cfa825a2951bd03bde834b7
JMChurchill <57601700+JMChurchill@users.noreply.github.com>
2022-01-20 12:22:38 +0000

    Completed flash cards

--- WebApp/persa-learn/src/Pages/Activities/FlashCard.js
+++ WebApp/persa-learn/src/Pages/Activities/FlashCard.js
@@ -1,6 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import Flashcard from "../../Components/Flashcard";
+
+import tempFlashcardData from "../../assets/tempFlashcardData.json";
 
 const FlashCard = () => {
+  const [currentCard, setCurrentCard] = useState(tempFlashcardData[0]);
+  const [count, setCount] = useState(1);
+  const nextCard = () => {
+    console.log(count);
+    setCount(count + 1);
+    if (count < tempFlashcardData.length) {
+      setCurrentCard(tempFlashcardData[count]);
+      console.log(count, currentCard);
+    } else {
+      alert("done");
+    }
+  };
+  const done = () => {
+    alert("done");
+  };
   return (
     <div className="content-box">
       <h1>Flash Card!</h1>
@@ -9,10 +27,20 @@ const FlashCard = () => {
           <div className="title">
             <h2>Cards Remaining: </h2>
           </div>
-          <div className="flash-card">
-            <p>Question</p>
-          </div>
+          <Flashcard
+            question={currentCard.question}
+            answer={currentCard.answer}
+          />
           <button className="btn">Flip card</button>
+          {count < tempFlashcardData.length ? (
+            <button onClick={nextCard} className="btn">
+              Next Card
+            </button>
+          ) : (
+            <button onClick={done} className="btn">
+              Done
+            </button>
+          )}
         </div>
       </div>
     </div>
