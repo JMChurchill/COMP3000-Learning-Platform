9ebffa11051be5154437a3859d0cf23eec20169b
JMChurchill <57601700+JMChurchill@users.noreply.github.com>
2022-03-09 20:19:55 +0000

    All flashcard screen

---
+++ MobileApp/persa-learn/app/components/revision/FlashCards/DeckItem.js
@@ -0,0 +1,39 @@
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import React from "react";
+import fonts from "../../../config/fonts";
+import common from "../../../config/common";
+import { useNavigation } from "@react-navigation/native";
+
+export default function DeckItem({ deckName = "Deck Name" }) {
+  const navigation = useNavigation();
+
+  const deckOnPress = () => {
+    navigation.navigate("FlashCardPlay");
+  };
+  return (
+    <TouchableOpacity
+      style={[styles.container, common.shadow]}
+      onPress={() => {
+        deckOnPress();
+      }}
+    >
+      <Text style={[fonts.h1, { fontWeight: "bold", color: "black" }]}>
+        {deckName}
+      </Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "yellow",
+    // flex: 2,
+    width: "90%",
+    // minHeight: "10%",
+    aspectRatio: 1 / 0.5,
+    margin: "2%",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: common.containerBorderRadius,
+  },
+});
--- MobileApp/persa-learn/app/screens/revision/FlashCard/FlashCardsScreen.js
+++ MobileApp/persa-learn/app/screens/revision/FlashCard/FlashCardsScreen.js
@@ -1,11 +1,43 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
 import React from "react";
 import fonts from "../../../config/fonts";
+import DeckItem from "../../../components/revision/FlashCards/DeckItem";
+import colors from "../../../config/colors";
+import { Entypo } from "@expo/vector-icons";
+import common from "../../../config/common";
+import { useNavigation } from "@react-navigation/native";
 
 export default function FlashCardsScreen() {
+  const navigation = useNavigation();
+
+  const createOnPress = () => {
+    navigation.navigate("FlashCardCreate");
+  };
   return (
     <View style={styles.root}>
-      <Text style={fonts.title}>FlashCardsScreen</Text>
+      <Text style={fonts.title}>All Flash Cards</Text>
+      <ScrollView style={{ width: "100%" }}>
+        <View style={styles.DeckContainer}>
+          <DeckItem />
+          <DeckItem />
+          <DeckItem />
+          <DeckItem />
+        </View>
+      </ScrollView>
+      <TouchableOpacity
+        style={[styles.buttonCreate, common.shadow]}
+        onPress={() => {
+          createOnPress();
+        }}
+      >
+        <Entypo name="plus" size={50} color="white" />
+      </TouchableOpacity>
     </View>
   );
 }
@@ -14,5 +46,26 @@ const styles = StyleSheet.create({
   root: {
     flex: 1,
     alignItems: "center",
+  },
+  DeckContainer: {
+    // flex: 1,
+    // borderWidth: 1,
+    width: "100%",
+    // flexDirection: "row",
+    // flexWrap: "wrap",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonCreate: {
+    position: "absolute",
+    bottom: 0,
+    right: 0,
+    justifyContent: "center",
+    alignItems: "center",
+    margin: 15,
+    borderRadius: 1000,
+    height: "10%",
+    aspectRatio: 1,
+    backgroundColor: colors.primary,
   },
 });
