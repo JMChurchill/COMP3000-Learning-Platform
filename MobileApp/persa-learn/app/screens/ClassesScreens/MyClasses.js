641ca7b82c95fb59b773710596280a6e91064089
Joshua Churchill <57601700+JMChurchill@users.noreply.github.com>
2022-07-08 11:44:14 +0100

    added padding to class view

--- MobileApp/persa-learn/app/screens/ClassesScreens/MyClasses.js
+++ MobileApp/persa-learn/app/screens/ClassesScreens/MyClasses.js
@@ -4,6 +4,7 @@ import {
   ActivityIndicator,
   FlatList,
   RefreshControl,
+  Text,
 } from "react-native";
 import React, { useContext, useState, useEffect, useCallback } from "react";
 
@@ -11,6 +12,7 @@ import * as SecureStore from "expo-secure-store";
 import { AuthContext } from "../../components/context";
 import ClassesItem from "../../components/Classes/ClassesItem";
 import { getClassesByStudent } from "../../httpRequests/classRequests";
+import fonts from "../../config/fonts";
 
 const MyClasses = () => {
   const { signOut } = useContext(AuthContext);
@@ -41,33 +43,49 @@ const MyClasses = () => {
   }, []);
   return (
     <View style={styles.root}>
-      <View style={styles.content}>
-        {false ? (
-          <ActivityIndicator size="large" color={colors.primary} />
-        ) : (
-          <View>
-            <FlatList
-              refreshControl={
-                <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
-              }
-              data={classes}
-              keyExtractor={({ id }, index) => id}
-              renderItem={({ item, separator }) => (
-                <ClassesItem
-                  key={item.ClassDetailsID}
-                  id={item.ClassDetailsID}
-                  name={item.Name}
-                  teacher={`${item.FirstName} ${item.LastName}`}
-                />
-              )}
-            />
-          </View>
-        )}
-      </View>
+      {false ? (
+        <ActivityIndicator size="large" color={colors.primary} />
+      ) : (
+        <>
+          <Text style={fonts.title}>My Classes</Text>
+
+          <FlatList
+            style={styles.allClassesContainer}
+            refreshControl={
+              <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+            }
+            data={classes}
+            keyExtractor={({ id }, index) => id}
+            renderItem={({ item, separator }) => (
+              <ClassesItem
+                key={item.ClassDetailsID}
+                id={item.ClassDetailsID}
+                name={item.Name}
+                teacher={`${item.FirstName} ${item.LastName}`}
+              />
+            )}
+          />
+        </>
+      )}
     </View>
   );
 };
 
 export default MyClasses;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    alignItems: "center",
+    padding: 0,
+  },
+  container: {
+    flex: 1,
+    margin: 0,
+    width: "100%",
+  },
+  allClassesContainer: {
+    width: "100%",
+    // paddingHorizontal: 5,
+  },
+});
