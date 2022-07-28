7b3e0e2872c90d646dd448f9a36e5ed4c0aeedab
Joshua Churchill <57601700+JMChurchill@users.noreply.github.com>
2022-07-28 20:26:08 +0100

    Modified host addresses

--- WebApp/persa-learn/src/config/hostAddress.js
+++ WebApp/persa-learn/src/config/hostAddress.js
@@ -5,6 +5,6 @@ export const hostAddress = () => {
   // return "http://localhost:8080";
   // return "http://178.62.104.116:8080";
   // return "http://178.62.104.116:8080";
-  console.log("api add: " + window.location.hostname);
+  // console.log("api add: " + window.location.hostname);
   return `http://${window.location.hostname}:8080`;
 };
