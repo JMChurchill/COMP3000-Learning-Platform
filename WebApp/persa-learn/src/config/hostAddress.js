763ad8aaa6099e895001376079c13e72362a09ba
Joshua Churchill <57601700+JMChurchill@users.noreply.github.com>
2022-07-30 10:37:15 +0100

    Improved docker automation

--- WebApp/persa-learn/src/config/hostAddress.js
+++ WebApp/persa-learn/src/config/hostAddress.js
@@ -1,4 +1,9 @@
 export const hostAddress = () => {
+  if (process.env.NODE_ENV != "production") {
+    return `http://${window.location.hostname}:8080`;
+  } else {
+    return "http://178.62.104.116:8080";
+  }
   // return local address
   // console.log(window.location.hostname);
   // return `http://${window.location.hostname}:8080`;
@@ -6,5 +11,5 @@ export const hostAddress = () => {
   // return "http://178.62.104.116:8080";
   // return "http://178.62.104.116:8080";
   // console.log("api add: " + window.location.hostname);
-  return `http://${window.location.hostname}:8080`;
+  // return `http://${window.location.hostname}:8080`;
 };
